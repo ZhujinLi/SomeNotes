bdf11fedc1cc9790155664c1adcdff5a7bc9b1ce
Zhujin Li <lizhujin@outlook.com>
2020-11-23 22:32:11 +0800

    Refine control bar

--- src/controlbar.cpp
+++ src/controlbar.cpp
@@ -3,11 +3,11 @@
 
 ControlBar::ControlBar(QWidget *parent) : QFrame(parent) {
     setFrameShape(QFrame::HLine);
-    setMinimumSize(QSize(50, 5));
+    setMinimumSize(QSize(50, 9));
     setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
     setFrameShadow(QFrame::Shadow::Sunken);
     setLineWidth(1);
-    setCursor(QCursor(Qt::CursorShape::OpenHandCursor));
+    setCursor(QCursor(Qt::CursorShape::ClosedHandCursor));
 }
 
 void ControlBar::mousePressEvent(QMouseEvent *) { emit(pressed()); }
